cd39271ac38c7cf9e2fb32f43c0fce1e758f78f8
李博闻 <libowen@libowendeMacBook-Pro.local>
2022-04-21 09:30:58 +0800

    no-1

---
+++ no-1.py
@@ -0,0 +1,11 @@
+# for循环
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+            for i in range(len(nums)):
+		x = nums[i]
+		for i in range(i+1, len(nums)):
+		    y = nums[i]
+		    if target == x + y
+			return{i, j]
+	    return[]
+
