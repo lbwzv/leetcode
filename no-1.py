b42e15a2dc422da346703e80dcce88d494f7c8b0
李博闻 <libowen@libowendeMacBook-Pro.local>
2022-04-21 16:44:16 +0800

    hashtable for no.1

--- no-1.py
+++ no-1.py
@@ -9,3 +9,14 @@ class Solution:
 			return{i, j]
 	    return[]
 
+
+# 哈希表
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+	hashtable = dict()
+	for i nums in enumerate(nums):
+# 这个函数的基本应用就是用来遍历一个集合对象，它在遍历的同时还可以得到当前元素的索引位置。
+	    if target - num  in hashtable:
+		return[hashtable[target - num], i]
+	    hashtable[num[i]] = i
+	return []
