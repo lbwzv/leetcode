0bac400682733ee19a7f817125180412df9304a4
李博闻 <libowen@192.168.1.3>
2022-06-02 19:26:19 +0800

    all clear

--- no-215.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        l = 0
-        while l < k - 1:
-            l += 1
-        return(nums[l])
